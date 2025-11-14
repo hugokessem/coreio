042c1dd9b163af0283484f885374dbe727fa27ed
Yohannes Teshome <kateyohannes@gmail.com>
2025-11-14 23:17:08 +0300

    latest

--- lib/ips/account_lookup/account_lookup.go
+++ lib/ips/account_lookup/account_lookup.go
@@ -3,96 +3,17 @@ package accountlookup
 import (
 	"encoding/xml"
 	"fmt"
+	"strings"
 )
 
-/*
-<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:mb="http://MB_IPS" xmlns:urn="urn:iso:std:iso:20022:tech:xsd:head.001.001.03" xmlns:urn1="urn:iso:std:iso:20022:tech:xsd:acmt.023.001.03">
-
-	<soapenv:Header/>
-	<soapenv:Body>
-	   <mb:AccountVerfication>
-	      <input1>
-	         <urn:AppHdr>
-	            <urn:Fr>
-	               <urn:FIId>
-	                  <urn:FinInstnId>
-	                     <urn:Othr>
-	                        <urn:Id>CBETETAA</urn:Id>
-	                     </urn:Othr>
-	                  </urn:FinInstnId>
-	               </urn:FIId>
-	            </urn:Fr>
-	            <urn:To>
-	               <urn:FIId>
-	                  <urn:FinInstnId>
-	                     <urn:Othr>
-	                        <urn:Id>FP</urn:Id>
-	                     </urn:Othr>
-	                  </urn:FinInstnId>
-	               </urn:FIId>
-	            </urn:To>
-	            <urn:BizMsgIdr>CBETETAA843572771</urn:BizMsgIdr>
-	            <urn:MsgDefIdr>acmt.023.001.03</urn:MsgDefIdr>
-	            <urn:CreDt>2023-06-24T00:00:00.000Z</urn:CreDt>
-	         </urn:AppHdr>
-	         <urn1:Document>
-	            <urn1:IdVrfctnReq>
-	               <urn1:Assgnmt>
-	                  <urn1:MsgId>CBETETAA843572771</urn1:MsgId>
-	                  <urn1:CreDtTm>2023-06-24T00:00:00.000+03:00</urn1:CreDtTm>
-	                  <urn1:Assgnr>
-	                     <urn1:Agt>
-	                        <urn1:FinInstnId>
-	                           <urn1:Othr>
-	                              <urn1:Id>CBETETAA</urn1:Id>
-	                           </urn1:Othr>
-	                        </urn1:FinInstnId>
-	                     </urn1:Agt>
-	                  </urn1:Assgnr>
-	                  <urn1:Assgne>
-	                     <urn1:Agt>
-	                        <urn1:FinInstnId>
-	                           <urn1:Othr>
-	                              <urn1:Id>ETSETAA</urn1:Id>
-	                           </urn1:Othr>
-	                        </urn1:FinInstnId>
-	                     </urn1:Agt>
-	                  </urn1:Assgne>
-	               </urn1:Assgnmt>
-	               <urn1:Vrfctn>
-	                  <urn1:Id>CBETETAA843572771</urn1:Id>
-	                  <urn1:PtyAndAcctId>
-	                     <urn1:Acct>
-	                        <urn1:Id>
-	                           <urn1:Othr>
-	                              <urn1:Id>1234567890</urn1:Id>
-	                              <urn1:SchmeNm>
-	                                 <urn1:Prtry>ACCT</urn1:Prtry>
-	                              </urn1:SchmeNm>
-	                           </urn1:Othr>
-	                        </urn1:Id>
-	                     </urn1:Acct>
-	                  </urn1:PtyAndAcctId>
-	               </urn1:Vrfctn>
-	            </urn1:IdVrfctnReq>
-	         </urn1:Document>
-	      </input1>
-	   </mb:AccountVerfication>
-	</soapenv:Body>
-
-</soapenv:Envelope>
-*/
 type Params struct {
-	AccountNumber               string
-	DebitBankBIC                string
-	CreaditBankBIC              string
-	CreditAccountNumber         string
-	BizMessageIdentifier        string
-	MessageDefinitionIdentifier string
-	MessageIdentifier           string
-	CreditDateTime              string
-	CreditDate                  string
-	CreaditAccountNumber        string
+	DebitBankBIC         string
+	CreaditBankBIC       string
+	CreditAccountNumber  string
+	BizMessageIdentifier string
+	MessageIdentifier    string
+	CreditDateTime       string
+	CreditDate           string
 }
 
 func NewAccountLookup(param Params) string {
@@ -169,19 +90,26 @@ func NewAccountLookup(param Params) string {
          </input1>
       </mb:AccountVerfication>
    </soapenv:Body>
-</soapenv:Envelope>`, param.DebitBankBIC, param.CreaditBankBIC, param.BizMessageIdentifier, param.CreditDate, param.MessageIdentifier, param.CreditDateTime, param.DebitBankBIC, param.CreaditBankBIC, param.MessageIdentifier, param.CreaditAccountNumber)
+</soapenv:Envelope>`, param.DebitBankBIC, param.CreaditBankBIC, param.BizMessageIdentifier, param.CreditDate, param.MessageIdentifier, param.CreditDateTime, param.DebitBankBIC, param.CreaditBankBIC, param.MessageIdentifier, param.CreditAccountNumber)
 }
 
-type Envelop struct {
+type Envelope struct {
 	XMLName xml.Name `xml:"Envelope"`
 	Body    Body     `xml:"Body"`
 }
 
 type Body struct {
-	AppHeader AppHeader `xml:"AppHdr"`
-	Document  Document  `xml:"Document"`
+	AccountVerficationResponse AccountVerficationResponse `xml:"AccountVerficationResponse"`
 }
 
+type AccountVerficationResponse struct {
+	Output Output `xml:"output1"`
+}
+
+type Output struct {
+	AppHeader *AppHeader `xml:"AppHdr"`
+	Document  *Document  `xml:"Document"`
+}
 type AppHeader struct {
 	From struct {
 		FIID struct {
@@ -228,5 +156,89 @@ type Document struct {
 				} `xml:"Agt"`
 			} `xml:"Assgne"`
 		} `xml:"Assgnmt"`
+		OrgnlAssgnmt *struct {
+			MessageId      string `xml:"MsgId"`
+			CreditDateTime string `xml:"CreDtTm"`
+		} `xml:"OrgnlAssgnmt"`
+		Rpt *struct {
+			OriginalIdentifier string `xml:"OrgnlId"`
+			Verfification      string `xml:"Vrfctn"`
+			Reason             struct {
+				Prtry string `xml:"Prtry"`
+			} `xml:"Rsn"`
+			OrgnlPtyAndAcctId *struct {
+				Acct *struct {
+					Id *struct {
+						Othr *struct {
+							Id      string `xml:"Id"`
+							SchmeNm *struct {
+								Prtry string `xml:"Prtry"`
+							} `xml:"SchmeNm"`
+						} `xml:"Othr"`
+					} `xml:"Id"`
+				} `xml:"Acct"`
+			} `xml:"OrgnlPtyAndAcctId"`
+			UpdtdPtyAndAcctId *struct {
+				Pty *struct {
+					Nm string `xml:"Nm"`
+				} `xml:"Pty"`
+				Acct *struct {
+					Id *struct {
+						Othr *struct {
+							Id      string `xml:"Id"`
+							SchmeNm *struct {
+								Prtry string `xml:"Prtry"`
+							} `xml:"SchmeNm"`
+						} `xml:"Othr"`
+					} `xml:"Id"`
+				} `xml:"Acct"`
+			} `xml:"UpdtdPtyAndAcctId"`
+		} `xml:"Rpt"`
 	} `xml:"IdVrfctnRpt"`
+}
+
+type AccountVerficationDetail struct {
+	CreaditBankBIC          string
+	OriginalIdentifier      string
+	CreditAccountNumber     string
+	CreditAccountHolderName string
+}
+
+type AccountVerficationResult struct {
+	Success bool
+	Deatil  *AccountVerficationDetail
+	Message []string
+}
+
+func ParseAccountLookupSOAP(xmlData string) (*AccountVerficationResult, error) {
+	var env Envelope
+	if err := xml.Unmarshal([]byte(xmlData), &env); err != nil {
+		return nil, err
+	}
+
+	if env.Body.AccountVerficationResponse.Output.AppHeader != nil && env.Body.AccountVerficationResponse.Output.Document != nil {
+		resp := env.Body.AccountVerficationResponse.Output
+		if strings.ToLower(resp.Document.IdVrfctnRpt.Rpt.Verfification) != "true" {
+			return &AccountVerficationResult{
+				Success: false,
+				Message: []string{"Account Not Found!"},
+			}, nil
+		}
+
+		return &AccountVerficationResult{
+			Success: true,
+			Deatil: &AccountVerficationDetail{
+				OriginalIdentifier:      resp.Document.IdVrfctnRpt.Rpt.OriginalIdentifier,
+				CreaditBankBIC:          resp.AppHeader.From.FIID.FinInstnId.Other.Identifier,
+				CreditAccountHolderName: resp.Document.IdVrfctnRpt.Rpt.UpdtdPtyAndAcctId.Pty.Nm,
+				CreditAccountNumber:     resp.Document.IdVrfctnRpt.Rpt.OrgnlPtyAndAcctId.Acct.Id.Othr.Id,
+			},
+		}, nil
+	}
+
+	return &AccountVerficationResult{
+		Success: false,
+		Message: []string{"Invalid Response!"},
+	}, nil
+
 }
---
+++ lib/ips/account_lookup/account_lookup_test.go
@@ -0,0 +1,43 @@
+package accountlookup
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccountLookupGeneratedXML(t *testing.T) {
+	test := []struct {
+		name   string
+		param  Params
+		expect []string
+	}{
+		{
+			name: "Validate Account Lookups",
+			param: Params{
+				CreditAccountNumber:  "1234567890",
+				DebitBankBIC:         "CBETETAA",
+				CreaditBankBIC:       "ETSETAA",
+				BizMessageIdentifier: "CBETETAA553572981",
+				MessageIdentifier:    "CBETETAA847772981",
+				CreditDateTime:       "2023-06-24T00:00:00.000+03:00",
+				CreditDate:           "2023-06-24T00:00:00.000Z",
+			},
+			expect: []string{
+				`<urn:BizMsgIdr>CBETETAA553572981</urn:BizMsgIdr>`,
+				`<urn:CreDt>2023-06-24T00:00:00.000Z</urn:CreDt>`,
+				`<urn1:MsgId>CBETETAA847772981</urn1:MsgId>`,
+				`<urn1:Id>1234567890</urn1:Id>`,
+			},
+		},
+	}
+
+	for _, tc := range test {
+		t.Run(tc.name, func(t *testing.T) {
+			xmlRequest := NewAccountLookup(tc.param)
+			for _, expectedStr := range tc.expect {
+				assert.Contains(t, xmlRequest, expectedStr)
+			}
+		})
+	}
+}
