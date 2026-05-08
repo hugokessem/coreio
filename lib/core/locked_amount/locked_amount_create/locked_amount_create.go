ac284d6d45fa9c38ecdec8f9891182a4bd97a74f
Yohannes Teshome <kateyohannes@gmail.com>
2026-05-08 02:09:19 -0700

    add more logs on create locked amount

--- lib/core/locked_amount/locked_amount_create/locked_amount_create.go
+++ lib/core/locked_amount/locked_amount_create/locked_amount_create.go
@@ -90,6 +90,9 @@ func ParseCreateLockedAmountSOAP(xmlData string) (*CreateLockedAmountResult, err
 	}
 
 	fmt.Println("response: ", env)
+	fmt.Println("body: ", env.Body)
+	fmt.Println("locedAmount: ", env.Body.CreateLockedAmountResponse)
+	fmt.Println("createdLockedAmountPaylod: ", env.Body.CreateLockedAmountResponse.CreateLockedAmount)
 	if env.Body.CreateLockedAmountResponse != nil {
 		resp := env.Body.CreateLockedAmountResponse
 		if resp.Status == nil {
