425498fe49fb0cf48de464e530edd9aeb67ec527
Yohannes Teshome <kateyohannes@gmail.com>
2026-08-11 01:13:54 -0700

    mastercard completed

--- master_card/init.go
+++ master_card/init.go
@@ -1,6 +1,8 @@
 package mastercard
 
 import (
+	"context"
+
 	customersupport "github.com/hugokessem/coreio/lib/master_card/customer_support"
 	"github.com/hugokessem/coreio/master_card/internal"
 )
@@ -25,15 +27,84 @@ type ActivityLog = customersupport.ActivityLog
 type CreateIssueParam = customersupport.CreateIssueParam
 type CreateIssueResult = customersupport.CreateIssueResult
 
-type MasterCardAPIInterface = customersupport.CustomerSupportAPIInterface
-type MasterCardCredential = customersupport.CustomerSupportCredential
+type MasterCardAPIInterface interface {
+	WorkerLogin(ctx context.Context, param WorkerLoginParam) (*WorkerLoginResult, error)
+	IssueProducts(ctx context.Context) (*IssueProductsResult, error)
+	IssuesList(ctx context.Context, param IssuesListParam) (*IssuesListResult, error)
+	ViewMessage(ctx context.Context, param ViewMessageParam) (*ViewMessageResult, error)
+	SendMessage(ctx context.Context, param SendMessageParam) (*SendMessageResult, error)
+	ExportActivityLog(ctx context.Context) (*ExportActivityLogResult, error)
+	CreateIssueForCustomer(ctx context.Context, param CreateIssueParam) (*CreateIssueResult, error)
+	SetAccessToken(token string)
+}
+
+type MasterCardCredential struct {
+	AuthURL     string
+	BaseURL     string
+	CustomerKey string
+	AccessToken string
+}
+
+type MasterCardAPI struct {
+	config  *internal.Config
+	support customersupport.CustomerSupportAPIInterface
+}
 
 func NewMasterCardAPI(param *MasterCardCredential) MasterCardAPIInterface {
-	config := internal.SetConfig(param.AuthURL, param.BaseURL, param.CustomerKey)
-	return customersupport.NewCustomerSupportAPI(customersupport.CustomerSupportCredential{
-		AuthURL:     config.AuthURL,
-		BaseURL:     config.BaseURL,
-		CustomerKey: config.CustomerKey,
-		AccessToken: param.AccessToken,
-	})
+	config := internal.SetConfig(
+		param.AuthURL,
+		param.BaseURL,
+		param.CustomerKey,
+		param.AccessToken,
+	)
+
+	return &MasterCardAPI{
+		config: config,
+		support: customersupport.NewCustomerSupportAPI(customersupport.CustomerSupportCredential{
+			AuthURL:     config.AuthURL,
+			BaseURL:     config.BaseURL,
+			CustomerKey: config.CustomerKey,
+			AccessToken: config.AccessToken,
+		}),
+	}
+}
+
+func (m *MasterCardAPI) SetAccessToken(token string) {
+	m.config.AccessToken = token
+	m.support.SetAccessToken(token)
+}
+
+func (m *MasterCardAPI) WorkerLogin(ctx context.Context, param WorkerLoginParam) (*WorkerLoginResult, error) {
+	result, err := m.support.WorkerLogin(ctx, param)
+	if err != nil {
+		return nil, err
+	}
+	if result != nil && result.AccessToken != "" {
+		m.config.AccessToken = result.AccessToken
+	}
+	return result, nil
+}
+
+func (m *MasterCardAPI) IssueProducts(ctx context.Context) (*IssueProductsResult, error) {
+	return m.support.IssueProducts(ctx)
+}
+
+func (m *MasterCardAPI) IssuesList(ctx context.Context, param IssuesListParam) (*IssuesListResult, error) {
+	return m.support.IssuesList(ctx, param)
+}
+
+func (m *MasterCardAPI) ViewMessage(ctx context.Context, param ViewMessageParam) (*ViewMessageResult, error) {
+	return m.support.ViewMessage(ctx, param)
+}
+
+func (m *MasterCardAPI) SendMessage(ctx context.Context, param SendMessageParam) (*SendMessageResult, error) {
+	return m.support.SendMessage(ctx, param)
+}
+
+func (m *MasterCardAPI) ExportActivityLog(ctx context.Context) (*ExportActivityLogResult, error) {
+	return m.support.ExportActivityLog(ctx)
+}
+
+func (m *MasterCardAPI) CreateIssueForCustomer(ctx context.Context, param CreateIssueParam) (*CreateIssueResult, error) {
+	return m.support.CreateIssueForCustomer(ctx, param)
 }
--- master_card/internal/config.go
+++ master_card/internal/config.go
@@ -4,15 +4,24 @@ type Config struct {
 	AuthURL     string
 	BaseURL     string
 	CustomerKey string
+	AccessToken string
 }
 
 var masterCardAPI *Config
 
-func SetConfig(authURL, baseURL, customerKey string) *Config {
+func SetConfig(authURL, baseURL, customerKey, accessToken string) *Config {
 	masterCardAPI = &Config{
 		AuthURL:     authURL,
 		BaseURL:     baseURL,
 		CustomerKey: customerKey,
+		AccessToken: accessToken,
+	}
+	return masterCardAPI
+}
+
+func GetConfig() *Config {
+	if masterCardAPI == nil {
+		panic("MasterCardAPI not initialized. Please call SetConfig() first.")
 	}
 	return masterCardAPI
 }
